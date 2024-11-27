96229a5c87e39f55496936b305496f7079c93c75
SavvinPC <savvinsergei2005@gmail.com>
2024-11-27 11:45:58 +0300

    fix: fix problems

--- getters.py
+++ getters.py
@@ -1,7 +1,17 @@
 import os
 import typing
 import requests
+import logging
 from schemas import AbstractErrorMessage
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
 
 BASE_URL = "https://tonapi.io/v2/"
 
@@ -13,14 +23,14 @@ def get_transaction_info(tx_hash: str) -> typing.Union[dict, AbstractErrorMessag
 
     for attempt in range(3):  
         try:
-            print(f"Attempting to fetch transaction info (Attempt {attempt + 1}): {url}")
-            response = requests.get(url, headers=headers, timeout=timeout)
+            logging.info(f"Attempting to fetch transaction info (Attempt {attempt + 1}): {url}")
+            response = requests.get(url, headers = headers, timeout = timeout)
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
-            print(f"Error fetching transaction info: {e}")
+            logging.error(f"Error fetching transaction info: {e}")
             if attempt == 2:
-                return AbstractErrorMessage(message="Error: Unable to fetch transaction info")
+                return AbstractErrorMessage(message = "Error: Unable to fetch transaction info")
             continue
 
 
@@ -32,8 +42,8 @@ def get_source_address(transaction_info: dict, jetton_type: str = None) -> typin
         except:
             return transaction_info["in_msg"]["source"]["address"]
     except KeyError as e:
-        print(f"Key error: {e}")
-        return AbstractErrorMessage(message="Error: Unable to get source address")
+        logging.error(f"Key error: {e}")
+        return AbstractErrorMessage(message = "Error: Unable to get source address")
 
 
 def is_jetton_wallet(address: str) -> typing.Union[bool, AbstractErrorMessage]:
@@ -44,16 +54,16 @@ def is_jetton_wallet(address: str) -> typing.Union[bool, AbstractErrorMessage]:
 
     for attempt in range(3):
         try:
-            print(f"Checking if address is a jetton wallet (Attempt {attempt + 1}): {url}")
+            logging.info(f"Checking if address is a jetton wallet (Attempt {attempt + 1}): {url}")
             response = requests.get(url, headers=headers, timeout=timeout)
             response.raise_for_status()
             result = response.json()
             interfaces = result.get("interfaces", [])
             return "jetton_wallet" in interfaces
         except requests.exceptions.RequestException as e:
-            print(f"Error checking jetton wallet: {e}")
+            logging.error(f"Error checking jetton wallet: {e}")
             if attempt == 2:
-                return AbstractErrorMessage(message="Error: Unable to determine if address is a jetton wallet")
+                return AbstractErrorMessage(message = "Error: Unable to determine if address is a jetton wallet")
             continue
 
 
@@ -68,10 +78,10 @@ def get_transfer_amount(transaction_info: dict, jetton_type: str, decimals: int 
             amount = decoded_body.get("amount")
             return float(amount) / (10 ** decimals) if amount else 0.0
         else:
-            return AbstractErrorMessage(message="Error: Unknown jetton type")
+            return AbstractErrorMessage(message = "Error: Unknown jetton type")
     except KeyError as e:
-        print(f"Key error when accessing transfer amount: {e}")
-        return AbstractErrorMessage(message="Error: Unable to get transfer amount")
+        logging.error(f"Key error when accessing transfer amount: {e}")
+        return AbstractErrorMessage(message = "Error: Unable to get transfer amount")
 
 
 def get_jetton_master(jetton_wallet_address: str) -> typing.Union[str, AbstractErrorMessage]:
@@ -82,19 +92,19 @@ def get_jetton_master(jetton_wallet_address: str) -> typing.Union[str, AbstractE
 
     for attempt in range(3):
         try:
-            print(f"Fetching jetton master (Attempt {attempt + 1}): {url}")
-            response = requests.get(url, headers=headers, timeout=timeout)
+            logging.info(f"Fetching jetton master (Attempt {attempt + 1}): {url}")
+            response = requests.get(url, headers = headers, timeout = timeout)
             response.raise_for_status()
             result = response.json()
             jetton_master = result.get("decoded", {}).get("jetton")
             if jetton_master:
                 return jetton_master
             else:
-                return AbstractErrorMessage(message="Error: Jetton master is None")
+                return AbstractErrorMessage(message = "Error: Jetton master is None")
         except requests.exceptions.RequestException as e:
-            print(f"Error fetching jetton master: {e}")
+            logging.error(f"Error fetching jetton master: {e}")
             if attempt == 2:
-                return AbstractErrorMessage(message="Error: Unable to fetch jetton master")
+                return AbstractErrorMessage(message = "Error: Unable to fetch jetton master")
             continue
 
 
@@ -106,32 +116,34 @@ def get_jetton_symbol(jetton_master: str) -> typing.Union[str, AbstractErrorMess
 
     for attempt in range(3):
         try:
-            print(f"Fetching jetton symbol (Attempt {attempt + 1}): {url}")
-            response = requests.get(url, headers=headers, timeout=timeout)
+            logging.info(f"Fetching jetton symbol (Attempt {attempt + 1}): {url}")
+            response = requests.get(url, headers = headers, timeout = timeout)
             response.raise_for_status()
             result = response.json()
             jetton_symbol = result.get("metadata", {}).get("symbol")
             if jetton_symbol:
                 return jetton_symbol
             else:
-                return AbstractErrorMessage(message="Error: Jetton symbol is None")
+                return AbstractErrorMessage(message = "Error: Jetton symbol is None")
         except requests.exceptions.RequestException as e:
-            print(f"Error fetching jetton symbol: {e}")
+            logging.error(f"Error fetching jetton symbol: {e}")
             if attempt == 2:
-                return AbstractErrorMessage(message="Error: Unable to fetch jetton symbol")
+                return AbstractErrorMessage(message = "Error: Unable to fetch jetton symbol")
             continue
 
 
 def get_transfer_payload(transaction_info: dict, jetton_type: str) -> typing.Union[str, AbstractErrorMessage]:
 
     try:
-        if jetton_type != "TON": payload_text = transaction_info["in_msg"]["decoded_body"]["forward_payload"]["value"]["value"]["text"]
-        else: payload_text = transaction_info["out_msgs"][0]["decoded_body"]["text"]
+        if jetton_type != "TON":
+            payload_text = transaction_info["in_msg"]["decoded_body"]["forward_payload"]["value"]["value"]["text"]
+        else:
+            payload_text = transaction_info["out_msgs"][0]["decoded_body"]["text"]
 
         return str(payload_text) or ""
     except KeyError as e:
-        print(f"Key error when accessing transfer payload: {e}")
-        return AbstractErrorMessage(message="Error: Unable to get transfer payload")
+        logging.error(f"Key error when accessing transfer payload: {e}")
+        return AbstractErrorMessage(message = "Error: Unable to get transfer payload")
 
 
 def get_jetton_decimals(jetton_master: str) -> typing.Union[int, AbstractErrorMessage]:
@@ -142,19 +154,19 @@ def get_jetton_decimals(jetton_master: str) -> typing.Union[int, AbstractErrorMe
 
     for attempt in range(3):
         try:
-            print(f"Fetching jetton decimals (Attempt {attempt + 1}): {url}")
+            logging.info(f"Fetching jetton decimals (Attempt {attempt + 1}): {url}")
             response = requests.get(url, headers=headers, timeout=timeout)
             response.raise_for_status()
             result = response.json()
             jetton_decimals = int(result["metadata"]["decimals"])
             return jetton_decimals
         except (KeyError, ValueError) as e:
-            print(f"Error parsing jetton decimals: {e}")
-            return AbstractErrorMessage(message="Error: Jetton decimals not found")
+            logging.error(f"Error parsing jetton decimals: {e}")
+            return AbstractErrorMessage(message = "Error: Jetton decimals not found")
         except requests.exceptions.RequestException as e:
-            print(f"Error fetching jetton decimals: {e}")
+            logging.error(f"Error fetching jetton decimals: {e}")
             if attempt == 2:
-                return AbstractErrorMessage(message="Error: Unable to fetch jetton decimals")
+                return AbstractErrorMessage(message = "Error: Unable to fetch jetton decimals")
             continue
 
 
@@ -166,17 +178,42 @@ def get_jetton_wallet_owner(jetton_wallet_address: str) -> typing.Union[str, Abs
 
     for attempt in range(3):
         try:
-            print(f"Fetching jetton master (Attempt {attempt + 1}): {url}")
-            response = requests.get(url, headers=headers, timeout=timeout)
+            logging.info(f"Fetching jetton master (Attempt {attempt + 1}): {url}")
+            response = requests.get(url, headers = headers, timeout = timeout)
             response.raise_for_status()
             result = response.json()
             owner_address = result.get("decoded", {}).get("owner")
             if owner_address:
                 return owner_address
             else:
-                return AbstractErrorMessage(message="Error: Owner address is None")
+                return AbstractErrorMessage(message = "Error: Owner address is None")
         except requests.exceptions.RequestException as e:
-            print(f"Error fetching owner address: {e}")
+            logging.error(f"Error fetching owner address: {e}")
             if attempt == 2:
-                return AbstractErrorMessage(message="Error: Unable to fetch owner address")
+                return AbstractErrorMessage(message = "Error: Unable to fetch owner address")
+            continue
+
+def get_sender_jetton_wallet(transaction_hash: str, source_address: str) -> typing.Union[str, AbstractErrorMessage]:
+    
+    url = f"{BASE_URL}accounts/{source_address}/events/{transaction_hash}"
+    headers = {'Accept': 'application/json'}
+    timeout = 30
+
+    for attempt in range(3):
+        try:
+            logging.info(f"Fetching jetton master (Attempt {attempt + 1}): {url}")
+            response = requests.get(url, headers = headers, timeout = timeout)
+            response.raise_for_status()
+            result = response.json()
+            sender_address = result["actions"][0]["JettonTransfer"]["senders_wallet"]
+
+            if sender_address:
+                return sender_address
+            else:
+                return AbstractErrorMessage(message = "Error: Owner address is None")
+            
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error fetching owner address: {e}")
+            if attempt == 2:
+                return AbstractErrorMessage(message = "Error: Unable to fetch owner address")
             continue
--- listener.py
+++ listener.py
@@ -19,15 +19,16 @@ logging.basicConfig(
 
 
 TON_API_KEY = "3751b902af2e0fccb69c5c279e23e0ed8dadadcc10a441e3fc3b43172595f4ea"
-ADMIN_WALLET_ADDRESS = Address("UQB4M_AbtopojI-EqoN9dfNZsSfFLcHZmYJQXP2_BIlazvxr").to_str(is_user_friendly=False)
+ADMIN_WALLET_ADDRESS = Address("UQB4M_AbtopojI-EqoN9dfNZsSfFLcHZmYJQXP2_BIlazvxr").to_str(is_user_friendly = False)
 
 
 async def send_response(response: NewTransferResponse):
+
     try:
         async with aiohttp.ClientSession() as session:
             async with session.post(
                 "https://xoma.monster/test/dep.php",
-                json=response
+                json = response
             ) as resp:
                 if resp.status != 200:
                     text = await resp.text()
@@ -37,12 +38,13 @@ async def send_response(response: NewTransferResponse):
 
 
 async def handle_message(event: TransactionEventData = None):
+
     try:
         transaction_hash = event.tx_hash
-        transaction_info = get_transaction_info(tx_hash=transaction_hash)
+        transaction_info = get_transaction_info(tx_hash = transaction_hash)
 
         if isinstance(transaction_hash, AbstractErrorMessage):
-            transaction_info = get_transaction_info(tx_hash=transaction_hash)
+            transaction_info = get_transaction_info(tx_hash = transaction_hash)
 
         source_address = get_source_address(transaction_info)
 
@@ -50,60 +52,64 @@ async def handle_message(event: TransactionEventData = None):
 
         if source_address != ADMIN_WALLET_ADDRESS:
 
-            jetton_wallet_flag = is_jetton_wallet(address=source_address)
+            jetton_wallet_flag = is_jetton_wallet(address = source_address)
 
             if jetton_wallet_flag:
-                jetton_wallet_owner = get_jetton_wallet_owner(jetton_wallet_address=source_address)
+
+                source_address = get_sender_jetton_wallet(transaction_hash = transaction_hash, source_address = source_address)
+                jetton_wallet_owner = get_jetton_wallet_owner(jetton_wallet_address = source_address)
 
                 if jetton_wallet_owner == ADMIN_WALLET_ADDRESS:
                     logging.info("Transfer from admin wallet")
                     return
 
                 transfer_type = "JETTON"
-                jetton_master = get_jetton_master(jetton_wallet_address=source_address)
-                jetton_symbol = get_jetton_symbol(jetton_master=jetton_master)
-                jetton_decimals = get_jetton_decimals(jetton_master=jetton_master)
-                amount = get_transfer_amount(transaction_info, jetton_type=transfer_type, decimals=jetton_decimals)
+                jetton_master = get_jetton_master(jetton_wallet_address = source_address)
+                jetton_symbol = get_jetton_symbol(jetton_master = jetton_master)
+                jetton_decimals = get_jetton_decimals(jetton_master = jetton_master)
+                amount = get_transfer_amount(transaction_info, jetton_type = transfer_type, decimals = jetton_decimals)
 
             else:
                 transfer_type = "TON"
                 jetton_symbol = None
                 jetton_master = None
-                amount = get_transfer_amount(transaction_info, jetton_type=transfer_type, decimals=9)
+                amount = get_transfer_amount(transaction_info, jetton_type = transfer_type, decimals = 9)
 
-            payload = get_transfer_payload(transaction_info, jetton_type=transfer_type)
+            payload = get_transfer_payload(transaction_info, jetton_type = transfer_type)
 
             response = NewTransferResponse(
-                type=transfer_type,
-                symbol=jetton_symbol or "null",
-                sender=source_address,
-                amount=amount,
-                payload_text=payload,
-                jetton_master_address=jetton_master,
-                hash=transaction_hash,
+                type = transfer_type,
+                symbol = jetton_symbol or "null",
+                sender = source_address,
+                amount = amount,
+                payload_text = payload,
+                jetton_master_address = jetton_master,
+                hash = transaction_hash,
             )
 
             logging.info(f"Response: {response}")
 
-            await send_response(response={
+            await send_response(response = {
                 "type": response.type,
                 "symbol": response.symbol,
                 "sender": response.sender,
                 "amount": response.amount,
                 "payload_text": response.payload_text,
                 "jetton_master_address": response.hash
-            })
+                }
+            )
     except Exception as e:
         logging.error(f"Error handling message: {e}")
 
 
 async def main():
+
     while True:
         try:
-            tonapi = AsyncTonapi(api_key=TON_API_KEY)
+            tonapi = AsyncTonapi(api_key = TON_API_KEY)
             accounts = ["UQB4M_AbtopojI-EqoN9dfNZsSfFLcHZmYJQXP2_BIlazvxr"]
 
-            await tonapi.websocket.subscribe_to_transactions(accounts=accounts, handler=handle_message)
+            await tonapi.websocket.subscribe_to_transactions(accounts = accounts, handler = handle_message)
 
             while True:
                 await asyncio.sleep(1)
